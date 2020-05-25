4e2a3af432451496a29a940e4ed55223767ebe4d
Paolo <paolo.facchinetti.ipad@gmail.com>
2020-05-25 16:24:02 +0200

    Poll creation only for Verified or Human identities

--- src/app/core/topbar/topbar.component.ts
+++ src/app/core/topbar/topbar.component.ts
@@ -4,6 +4,7 @@ import {select, Store} from '@ngrx/store';
 import {getSession, State} from '@app-redux/index';
 import {SessionBean} from '@app-shared/model/session.bean';
 import {filter} from 'rxjs/operators';
+import {StatusEnum} from '@app-shared/model/status.enum';
 
 @Component({
   selector: 'app-topbar',
@@ -12,12 +13,22 @@ import {filter} from 'rxjs/operators';
 })
 export class TopbarComponent implements OnInit {
   session: SessionBean;
+  canCreate: boolean;
 
   constructor(protected dataService: DataService, protected store: Store<State>) {
     this.store.pipe(select(getSession), filter((p) => p != null)).subscribe( (s) => {
       this.session = s;
     });
     this.dataService.getSessionOnlyCheck();
+
+    /**
+    Conditions for being able to create a new poll
+     */
+    if(this.session.status==StatusEnum.HUMAN || this.session.status == StatusEnum.VERIFIED){
+      this.canCreate = true;
+    }else{
+      this.canCreate = false;
+    }
   }
 
   ngOnInit(): void {
