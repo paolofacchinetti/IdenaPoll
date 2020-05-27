073faa3ff773dc7c37d0bfbfabdf69d947bdf8ab
Paolo <paolo.facchinetti.ipad@gmail.com>
2020-05-27 17:57:07 +0200

    added create component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TopbarComponent } from './topbar/topbar.component';
-import {FlexModule} from "@angular/flex-layout";
-import { SigninComponent } from './signin/signin.component';
-import {SharedModule} from "@app-shared/shared.module";
-import { HomeComponent } from './home/home.component';
-import {RouterModule} from "@angular/router";
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TopbarComponent} from './topbar/topbar.component';
+import {FlexModule} from '@angular/flex-layout';
+import {SigninComponent} from './signin/signin.component';
+import {SharedModule} from '@app-shared/shared.module';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
+import {CreateComponent} from './create/create.component';
 
 
 @NgModule({
-  declarations: [TopbarComponent, SigninComponent, HomeComponent],
+  declarations: [TopbarComponent, SigninComponent, HomeComponent, CreateComponent],
   exports: [
     TopbarComponent
   ],
-    imports: [
-        CommonModule,
-        FlexModule,
-        SharedModule,
-        RouterModule
-    ]
+  imports: [
+    CommonModule,
+    FlexModule,
+    SharedModule,
+    RouterModule
+  ]
 })
 export class CoreModule { }
