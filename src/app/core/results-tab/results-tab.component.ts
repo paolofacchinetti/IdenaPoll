5d85c350b60ebf501cbe3c73a5539023cc67a0ef
Paolo <paolo.facchinetti.ipad@gmail.com>
2020-06-05 18:46:38 +0200

    fix graphs axes values

--- src/app/core/results-tab/results-tab.component.ts
+++ src/app/core/results-tab/results-tab.component.ts
@@ -96,9 +96,10 @@ export class ResultsTabComponent implements OnInit, AfterViewInit{
         },
         options: {
           scales: {
-            yAxes: [{
+            xAxes: [{
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
+                stepSize: 1
               }
             }]
           }
@@ -176,9 +177,10 @@ export class ResultsTabComponent implements OnInit, AfterViewInit{
         },
         options: {
           scales: {
-            yAxes: [{
+            xAxes: [{
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
+                stepSize: 1
               }
             }]
           }
@@ -276,9 +278,10 @@ export class ResultsTabComponent implements OnInit, AfterViewInit{
         },
         options: {
           scales: {
-            yAxes: [{
+            xAxes: [{
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
+                stepSize: 1
               }
             }]
           }
@@ -292,22 +295,8 @@ export class ResultsTabComponent implements OnInit, AfterViewInit{
         datasets: [{
           label: 'Voters',
           data: this.resultsPoll.ageData,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
+          backgroundColor: [],
+          borderColor: [],
           borderWidth: 1
         }]
       },
@@ -315,7 +304,8 @@ export class ResultsTabComponent implements OnInit, AfterViewInit{
         scales: {
           yAxes: [{
             ticks: {
-              beginAtZero: true
+              beginAtZero: true,
+              stepSize: 1
             }
           }]
         }
